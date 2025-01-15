bf634e29fe1cacbc1a58140ee9bb6a7405423b30
sla89 <4637490+sla89@users.noreply.github.com>
2025-01-15 20:04:30 +0100

    Added custom log formatter.

--- can2mqtt_core/can2mqtt_core/Program.cs
+++ can2mqtt_core/can2mqtt_core/Program.cs
@@ -1,8 +1,16 @@
 ﻿using can2mqtt;
+using Microsoft.Extensions.Logging.Console;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
+        services.AddLogging(logging =>
+        {
+            logging.ClearProviders();
+            logging.AddConsole();
+            logging.AddConsoleFormatter<Can2MqttLogFormatter, ConsoleFormatterOptions>();
+        });
+
         services.AddHostedService<Can2Mqtt>();
     })
     .Build();
